89f16d5096af8b33b5a829f3291c301511f75d07
Ayush Kothari <ayush@croxel.com>
2024-06-17 16:37:34 +0530

    [CXRD-18] Add LED blink upon startup
    Add GPIO LED blink upon startup
    
    Signed-off-by: Ayush Kothari <ayush@croxel.com>

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/gpio.h>
 
 LOG_MODULE_REGISTER(main);
 
 static const struct pwm_dt_spec pwm_buzzer = PWM_DT_SPEC_GET(DT_ALIAS(buzzer_pwm));
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
 void play_starting_seq(void)
 {
@@ -22,19 +25,32 @@ void play_starting_seq(void)
 			LOG_ERR("err in pwm_set_dt");
 			return;
 		}
+		gpio_pin_set_dt(&led0, true);
+		gpio_pin_set_dt(&led1, false);
+
 		k_msleep(250);
 		if (0 != pwm_set_dt(&pwm_buzzer, PWM_HZ(4000), 0)) {
 			LOG_ERR("err in pwm_set_dt");
 			return;
 		}
+		gpio_pin_set_dt(&led0, false);
+		gpio_pin_set_dt(&led1, true);
 		k_msleep(250);
 	}
+	gpio_pin_set_dt(&led1, false);
 }
 
 int main(void)
 {
 	if (!pwm_is_ready_dt(&pwm_buzzer)) {
 		LOG_ERR("Error: PWM device %s is not ready\n", pwm_buzzer.dev->name);
+		return 0;
+	}
+	if (!gpio_is_ready_dt(&led0) || !gpio_is_ready_dt(&led1)) {
+		return 0;
+	}
+	if ((gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE) < 0) ||
+	    gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE)) {
 		return 0;
 	}
 
