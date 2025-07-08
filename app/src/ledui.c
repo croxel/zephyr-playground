ead400751e602955918ffcd32c9b0936519de994
Anuj Pathak <anuj@croxel.com>
2025-07-08 17:28:03 +0530

    [CXRD-19] Added LP5521 support
    - added required patches and enabled lp5521 usage in sample
    
    Signed-off-by: Anuj Pathak <anuj@croxel.com>

--- app/src/ledui.c
+++ app/src/ledui.c
@@ -2,6 +2,8 @@
 #include <zephyr/logging/log.h>
 #include <user_alerts/user_alerts.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/dt-bindings/led/led.h>
 #include <user_button_actions/user_button_actions.h>
 
 LOG_MODULE_REGISTER(ledui);
@@ -57,26 +59,77 @@ static struct user_alerts_channel _bi_led_ch = {
 	.pattern = NULL,
 };
 
-const struct bi_led_alert_step _btn_click_bi_led_steps[] = {
+int rgb_led_user_alerts_step_exec(const struct user_alerts_channel *ch, const void *step)
+{
+	int err;
+	const struct rgb_led_alert_step *b_step = step;
+	const struct device *const dev = ch->io;
+
+	if (b_step) {
+		err = led_set_brightness(dev, LED_COLOR_ID_RED, b_step->r);
+		err = led_set_brightness(dev, LED_COLOR_ID_GREEN, b_step->g);
+		err = led_set_brightness(dev, LED_COLOR_ID_BLUE, b_step->b);
+	} else {
+		err = led_off(dev, LED_COLOR_ID_RED);
+		err = led_off(dev, LED_COLOR_ID_GREEN);
+		err = led_off(dev, LED_COLOR_ID_BLUE);
+	}
+	return err;
+}
+
+const struct rgb_led_alert_step _btn_click_rgb_led_steps[] = {
+	{.ms = 250, .r = 0x64, .g = 0x64, .b = 0x64},
+	{.ms = 250, .r = 0x64, .g = 0x64, .b = 0x00},
+	{.ms = 250, .r = 0x64, .g = 0x00, .b = 0x64},
+	{.ms = 250, .r = 0x64, .g = 0x00, .b = 0x00},
+	{.ms = 250, .r = 0x00, .g = 0x64, .b = 0x64},
+	{.ms = 250, .r = 0x00, .g = 0x64, .b = 0x00},
+	{.ms = 250, .r = 0x00, .g = 0x00, .b = 0x64},
+	{.ms = 250, .r = 0x00, .g = 0x00, .b = 0x00},
+};
+struct user_alerts_pattern _btn_click_rgb_led_pattern = {
+	.steps = _btn_click_rgb_led_steps,
+	.steps_count = ARRAY_SIZE(_btn_click_rgb_led_steps),
+	.loop_count = 2,
+};
+
+const struct device *const _rgb_led_io = DEVICE_DT_GET_ANY(ti_lp5521);
+static struct user_alerts_channel _rgb_led_ch = {
+	.step_size = sizeof(struct rgb_led_alert_step),
+	.io = _rgb_led_io,
+	.exec = rgb_led_user_alerts_step_exec,
+	.cur_step_idx = 0,
+	.cur_loop_idx = 0,
+	/*".timer" wil be init using api */
+	.pattern = NULL,
+
+};
+
+const struct bi_led_alert_step _btn_press_bi_led_steps[] = {
 	{.ms = 50, .duty_1 = 0xFF, .duty_2 = 0x00},
 	{.ms = 450, .duty_1 = 0x00, .duty_2 = 0x00},
 	{.ms = 50, .duty_1 = 0x00, .duty_2 = 0xFF},
 	{.ms = 450, .duty_1 = 0x00, .duty_2 = 0x00},
 };
-struct user_alerts_pattern _btn_click_bi_led_pattern = {
-	.steps = _btn_click_bi_led_steps,
-	.steps_count = ARRAY_SIZE(_btn_click_bi_led_steps),
+struct user_alerts_pattern _btn_press_bi_led_pattern = {
+	.steps = _btn_press_bi_led_steps,
+	.steps_count = ARRAY_SIZE(_btn_press_bi_led_steps),
 	.loop_count = 0,
 };
 
 void led_btn_action_handler(struct user_button_actions_channel *ch)
 {
-	if (ch->result.code == eUSER_BUTTON_CLICK) {
-		if (_bi_led_ch.pattern == &_btn_click_bi_led_pattern) {
+	switch (ch->result.code) {
+		case eUSER_BUTTON_PRESS: {
+		if (_bi_led_ch.pattern == &_btn_press_bi_led_pattern) {
 			user_alerts_channel_stop(&_bi_led_ch, true);
 		} else {
-			user_alerts_channel_play(&_bi_led_ch, &_btn_click_bi_led_pattern, true);
+			user_alerts_channel_play(&_bi_led_ch, &_btn_press_bi_led_pattern, true);
 		}
+		} break;
+		case eUSER_BUTTON_CLICK: {
+			user_alerts_channel_play(&_rgb_led_ch, &_btn_click_rgb_led_pattern, true);
+		} break;
 	}
 }
 
@@ -116,6 +169,9 @@ int play_bootup_led_blinks(void)
 
 	user_alerts_channel_init_timer(&_bi_led_ch);
 	user_alerts_channel_play(&_bi_led_ch, &_bootup_bi_led_pattern, true);
+
+	user_alerts_channel_init_timer(&_rgb_led_ch);
+
 	return 0;
 }
 
